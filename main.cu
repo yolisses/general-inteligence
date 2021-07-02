948304052fc0e7424f4895aad23964a98f58cba4
Ulisses <m.stunik@gmail.com>
2021-07-02 09:57:11 -0300

    creates a calling cascade

--- main.cu
+++ main.cu
@@ -4,6 +4,8 @@
 
 #define SIZE 10
 
+#define TYPE_AND 0
+
 #define cudaCheckError()                                                                       \
     {                                                                                          \
         cudaError_t e = cudaGetLastError();                                                    \
@@ -18,7 +20,13 @@ __device__ Node *nodes;
 
 struct Node
 {
+    char actualIndex;
+    char type;
+    bool alreadyUsed = false;
+    float lastValue;
+
     Node *childs[4];
+    Node *calledBy;
 
     __device__ int id()
     {
@@ -40,8 +48,65 @@ __global__ void initializeRandom()
     curand_init(clock64(), i * blockDim.x + j, 0, &state);
 
     nodes[i].childs[j] = &nodes[curand(&state) % SIZE];
-
     // printf("nodes[%d].child[%d] = %d\n", i, j, nodes[i].childs[j]->id());
+
+    nodes[i].calledBy = &nodes[curand(&state) % SIZE]; //impede receive para algo fora da memoria
+
+    nodes[i].lastValue = i * 111; //marcador do tipo Node[3].lastvalue = 333
+
+    nodes[i].alreadyUsed = false; //por segurança
+
+    nodes[i].type = TYPE_AND; //para testes iniciais
+}
+
+__global__ void call(Node *caller);
+
+__global__ void receive(Node *receiver, float value)
+{
+    receiver->actualIndex++;
+
+    printf("Node[%d] (and) recebeu valor %.0f\n", receiver->id(), value);
+
+    switch (receiver->type)
+    {
+    //Aqui tem maior ou igual porquw actualIndex varia bastante
+    case TYPE_AND:
+        if (receiver->actualIndex >= 4)
+        {
+            receiver->actualIndex = 0;
+            receive<<<1, 1>>>(receiver->calledBy, value);
+            // printf("Node[%d] (and) recebeu quatro vezes\n", receiver->id());
+        }
+        break;
+    }
+    receiver->lastValue = value;
+    // receiver->alreadyUsed = false; //allow node to be called several times
+}
+
+__global__ void call(Node *caller)
+{
+    caller->alreadyUsed = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (!caller->childs[i]->alreadyUsed)
+        {
+            caller->childs[i]->alreadyUsed = true;
+            caller->childs[i]->calledBy = caller;
+            call<<<1, 1>>>(caller->childs[i]);
+            // printf("Node[%d], type %d, call Node[%d]\n", caller->id(), caller->type, caller->childs[i]->id());
+        }
+        else
+        {
+            receive<<<1, 1>>>(caller, caller->childs[i]->lastValue);
+            printf("Node[%d] tentou chamar Node[%d] usado, recebe %.0f\n", caller->id(), caller->childs[i]->id(), caller->childs[i]->lastValue);
+        }
+    }
+}
+
+__global__ void callFirst()
+{
+    call<<<1, 1>>>(nodes);
 }
 
 int main()
@@ -50,7 +115,8 @@ int main()
     cudaDeviceSynchronize();
     initializeRandom<<<SIZE, 4>>>();
     cudaDeviceSynchronize();
-
+    callFirst<<<1, 1>>>();
+    cudaDeviceSynchronize();
     cudaCheckError();
 }
 
